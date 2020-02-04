d617e825f6e110a08b0beae23f54f07a1511ee92
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2020-02-04 17:28:15 -0600

    Main file set up with logic
    Co-authored-by: Kathryn Rhoads <kathryn.ann.rhoads@gmail.com>
    Co-authored-by: Michael Hart <48599443+worksofhart@users.noreply.github.com>
    Co-authored-by: Dora Belme <contact.dorabelme@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import time
 import requests
+import hashlib
 
-from traversal import take_treasure, status_inventory, sell_treasure, wise_explorer, movement, name_changer
+# from traversal import take_treasure, status_inventory, sell_treasure, wise_explorer, movement, name_changer, pray
+from mine import proof_of_work, valid_proof
 from graphutils import bfs
 from player import Player
 
@@ -24,55 +26,98 @@ while player.gold < 1000 and player.name is "User":
     ## On the way to random room, we need to examine the room each time we enter a new one. So if new room, call function examine
     ## if there are items in the room, then we take those items up until we have 9 items.
     ## When we hit 9 items, we should return to the shop from our current room.
-    movement()
     handle_items()
-    prev_room = player.current_room
 
 
 ## Go back to the shop and sell the item
-path = bfs(player.current_room, 1)
+# traversal = bfs(player.current_room, 1)
+# move_to_location(traversal)
 ## Try to use wise explorer instead of move endpoint
-for m in path:
-    room = player.current_room
-    exits = map.json[room]["exits"]
-    for direction, roomID in exits:
-        if roomID == m:
-            wise_explorer(direction, API_KEY, roomID )
+# for m in traversal:
+    # room = player.current_room
+    # exits = map.json[room]["exits"]
+    # for direction, roomID in exits:
+    #     if roomID == m:
+    # response = wise_explorer(m[0], API_KEY, m[1])
+    # cooldown = response["cooldown"]
+    # time.sleep(cooldown)
+    # if "errors" in response:
+    #     print(response["errors"])
 
-sell_treasure(player.inventory, API_KEY)
+## Sell the item
+# for item in player.inventory:
+#     sell_treasure(item, API_KEY)
 
 
 ## While 1000 gold, make way to pirate ry.
 while player.gold >= 1000:
     path = bfs(player.current_room, 467)
-    for m in path:
-    room = player.current_room
-    exits = map.json[room]["exits"]
-    for direction, roomID in exits:
-        if roomID == m:
-            wise_explorer(direction, API_KEY, roomID)
+    move_to_location(path)
+    # for m in path:
+    # room = player.current_room
+    # exits = map.json[room]["exits"]
+    # for direction, roomID in exits:
+    #     if roomID == m:
+        # wise_explorer(m[0], API_KEY, m[1])
+        # cooldown = response["cooldown"]
+        # time.sleep(cooldown)
+        # if "errors" in response:
+        #     print(response["errors"])
             
-    ## At pirate ry, buy name.
+    ## At pirate ry, change name.
     name_changer(name, API_KEY)
+    cooldown = response["cooldown"]
+    time.sleep(cooldown)
 
 ## Go to the shrine, and use pray function
+path = bfs(player.current_room, 374)
+move_to_location(path)
+pray(API_KEY)
+
+path = bfs(374, 461)
+move_to_location(path)
+pray(API_KEY)
+
+
 
 
 ## Move from pirate ry to the well to solve puzzle with ls-8
+path = bfs(461, 55)
+move_to_location(path)
+## Solve the puzzle
+
 ## Move from the well to the new location
 ## Mine at new location
+response = requests.get("https://lambda-treasure-hunt.herokuapp.com/api/bc/last_proof/", headers=header)
+last_bl = response.json()["proof"]
+new_proof = proof_of_work(last_bl)
+data = json.dumps({"proof": new_proof})
+response = requests.post("https://lambda-treasure-hunt.herokuapp.com/api/bc/mine/", headers=header, data=data)
 
 
 def handle_items():
     ## Have to change from map.json? YES
-    if len(map.json["items"]) > 0:
+    if response["items"]:
         first_response = status_inventory(API_KEY)
-        if len(first_response.json()["inventory"]) > 9:
+        if len(first_response["inventory"]) > 9:
             # change path player.room_id to the shop room_id: 1
-            graph.bfs(player.room_id, 1)
-        for item in map.json["items"]:
+            path = graph.bfs(player.room_id, 1)
+            move_to_location(path)
+            for item in player.inventory:
+                response = sell_treasure(item, API_KEY)
+                cooldown = response["cooldown"]
+                time.sleep(cooldown)
+        for item in response["items"]:
             response = take_treasure(item, API_KEY)
-            cooldown = response.json()["cooldown"]
+            player.add_to_inventory(item)
+            cooldown = response["cooldown"]
             time.sleep(cooldown)
             break
 
+def move_to_location(path):
+    for m in path:
+        wise_explorer(m[0], API_KEY, m[1])
+        cooldown = response["cooldown"]
+        time.sleep(cooldown)
+        if "errors" in response:
+            print(response["errors"])
--- player.py
+++ player.py
@@ -1,6 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+
 class Player:
     def __init__(self, name='User', starting_room):
         self.name = name,
+        self.self.API_KEY,
         self.current_room = starting_room,
         self.cooldown = 1,
         self.encumbrance: 2,
@@ -13,3 +21,336 @@ class Player:
         self.status = [],
         self.errors = [],
         self.messages = [],
+
+    def add_to_inventory(self, item):
+        self.inventory.append(item)
+
+
+
+
+    def movement(self, direction):
+        movement_header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/",
+                    headers=movement_header,
+                    data=json.dumps({"direction": direction}),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def wise_explorer(self, direction, room_id):
+        movement_header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/",
+                    headers=movement_header,
+                    data=json.dumps({"direction": direction, "next_room_id": str(room_id)})
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def take_treasure(self, treasure):
+        treasure_header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/take/",
+                    headers=treasure_header,
+                    data=json.dumps({"name": f"{treasure}"}),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def drop_treasure(self, treasure):
+        treasure_header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/drop/",
+                    headers=treasure_header,
+                    data=json.dumps({"name": f"{treasure}"}),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def sell_treasure(self, treasure):
+        treasure_header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/",
+                    headers=treasure_header,
+                    data=json.dumps({"name": f"{treasure}", "confirm": "yes"}),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def status_inventory(self):
+        inventory_header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/status/",
+                    headers=inventory_header,
+                    data=json.dumps(),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def examine(self, treasure, player):
+        inventory_header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/examine/",
+                    headers=inventory_header,
+                    data=json.dumps({"name":f"{treasure or player}"}),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def name_changer(self, name):
+        header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/",
+                    headers=header,
+                    data=json.dumps({"name":f"{name}"}),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def pray(self):
+        header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/pray/",
+                    headers=header,
+                    data=json.dumps(),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def flight(self, direction):
+        header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+        response = requests.post(
+            "https://lambda-treasure-hunt.herokuapp.com/api/adv/fly/",
+            headers=header,
+            data=json.dumps({"direction":f"{direction}"}),
+        )
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/fly/",
+                    headers=header,
+                    data=json.dumps({"direction":f"{direction}"}),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def dash(self, direction, num_rooms, room_id):
+        header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/dash/",
+                    headers=header,
+                    data=json.dumps({"direction":f"{direction}", "num_rooms": f"{num_rooms}", "next_room_ids":f"{room_id}"}),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def carry(self, treasure):
+        header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/carry/",
+                    headers=header,
+                    data=json.dumps({"name":f"{treasure}"}),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+        
+    def receive(self):
+        header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/receive/",
+                    headers=header,
+                    data=json.dumps(),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
+
+    def warp(self):
+        header = {
+            "Authorization": f"Token {self.API_KEY}",
+            "Content-Type": "application/json",
+        }
+
+        successful = False
+        while not successful:
+            try:
+                response = requests.post(
+                    "https://lambda-treasure-hunt.herokuapp.com/api/adv/warp/",
+                    headers=header,
+                    data=json.dumps(),
+                ).json()
+                successful = True
+            except ValueError:
+                print("Error:  Non-json response")
+                print("Response returned:")
+                print(response)
+                time.sleep(self.cooldown)
+                
+        return response
