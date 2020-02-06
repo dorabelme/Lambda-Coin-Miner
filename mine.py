cf4de3c900724d40491121bf587d153f179cde6e
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2020-02-05 21:11:09 -0600

    Fixed mining difficulty
    Co-authored-by: Kathryn Rhoads <kathryn.ann.rhoads@gmail.com>
    Co-authored-by: Michael Hart <48599443+worksofhart@users.noreply.github.com>

--- mine.py
+++ mine.py
@@ -11,4 +11,4 @@ def proof_of_work(last_block, difficulty):
 def valid_proof(bl_string, proof, difficulty):
     guess = f"{bl_string}{proof}".encode()
     guess_hash = hashlib.sha256(guess).hexdigest()
-    return guess_hash[:DIFFICULTY] == "0" * DIFFICULTY
+    return guess_hash[:difficulty] == "0" * difficulty
